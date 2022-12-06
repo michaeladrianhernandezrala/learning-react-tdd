a10b74de6635303435c30e08f513d91dc0a68866
Michael Hernandez <michaeladrian.hernandez.rala@gmail.com>
2022-12-06 22:16:31 +0100

    Refactor test

--- src/test/Appointment.test.js
+++ src/test/Appointment.test.js
@@ -5,23 +5,27 @@ import { act } from "react-dom/test-utils";
 import { Appointment } from "../Appointment";
 
 describe("Appointment", () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
   it("Renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
 
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(component);
     expect(document.body.textContent).toContain("Ashley");
   });
 
   it("Renders another the customer first name", () => {
     const customer = { firstName: "Jordan" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
 
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(component);
     expect(document.body.textContent).toContain("Jordan");
   });
 });
